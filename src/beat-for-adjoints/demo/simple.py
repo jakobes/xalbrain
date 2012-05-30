c56f7599158f9b86c0b0fcaae70425d9960a0b55
Marie E. Rognes <meg@simula.no>
2012-05-30 16:10:24 +0200

    Get stuck with various 'need a value errors'

--- src/beat-for-adjoints/demo/simple.py
+++ src/beat-for-adjoints/demo/simple.py
@@ -18,42 +18,67 @@ class MyHeart(CardiacModel):
         M_e = 1.0
         return (M_i, M_e)
 
+parameters["adjoint"]["test_derivative"] = True
+
 cell_parameters = {"epsilon": 0.01, "gamma": 0.5, "alpha": 0.1}
 cell = FitzHughNagumo(cell_parameters)
 heart = MyHeart(cell)
 
-parameters = Parameters()
-parameters.add("theta", 0.5)
-parameters.add("enable_adjoint", True)
-solver = SplittingSolver(heart, parameters)
+application_parameters = Parameters()
+application_parameters.add("theta", 0.5)
+application_parameters.add("enable_adjoint", True)
 
-# Set initial conditions
+solver = SplittingSolver(heart, application_parameters)
+
+def main(vs0):
+    (vs_, vs, u) = solver.solution_fields()
+    vs_.assign(vs0, annotate=application_parameters["enable_adjoint"])
+    solver.solve((0, 0.1), 0.05)
+    return (vs, vs_)
+
+# Define initial conditions
 vs_expr = Expression(("- x[0]*(1-x[0])*x[1]*(1-x[1])", "0.0"))
-vs0 = project(vs_expr, solver.VS,
-              annotate=parameters["enable_adjoint"])
-(vs_, vs, u) = solver.solution_fields()
-vs_.assign(vs0, annotate=parameters["enable_adjoint"])
+vs0 = project(vs_expr, solver.VS)
+
+# Run main stuff
+info_green("Solving primal")
+(vs, vs_) = main(vs0)
 
 # Just quick regression test, not validation
-info_green("Solving primal")
-solver.solve((0, 0.1), 0.01)
-print "-"*80
-ref = a = 0.023771346883295
-a = norm(vs.split()[0])
-print "a = %.15f" % a
-diff = abs(a - ref)
-assert diff < 1.e-9, "a = %g, diff = %g" % (a, diff)
-print "-"*80
+regressiontest = False
+if regressiontest:
+    print "-"*80
+    ref = 0.023771346883295
+    a = norm(vs.split()[0])
+    diff = abs(a - ref)
+    assert diff < 1.e-9, "a = %g, diff = %g" % (a, diff)
+    print "-"*80
 
 # Try replaying forward
-adj_html("forward.html", "forward")
-adj_html("adjoint.html", "adjoint")
-info_green("Replaying")
-success = replay_dolfin(tol=1.e-10, stop=True)
+replay = False
+if replay:
+    adj_html("forward.html", "forward")
+    adj_html("adjoint.html", "adjoint")
+    info_green("Replaying")
+    success = replay_dolfin(tol=1.e-10, stop=True, forget=False)
 
 # Try computing some kind of adjoint
-J = FinalFunctional(inner(vs, vs)*dx)
-ic_param = InitialConditionParameter(vs_)
-dJdic = compute_gradient(J, ic_param)
+j = inner(vs, vs)*dx
+J = FinalFunctional(j)
+parameters["adjoint"]["stop_annotating"] = True # stop registering equations
 
+ic_param = InitialConditionParameter(vs_) # This "works"
+#ic_param = InitialConditionParameter(vs0) # This gives None
+dJdic = compute_gradient(J, ic_param)
 plot(dJdic, interactive=True)
+
+def Jhat(vs0):
+    (vs, vs_) = main(vs0)
+    j = inner(vs, vs)*dx
+    return assemble(j)
+
+Jvalue = assemble(j)
+print "Jvalue = ", Jvalue
+#
+conv_rate = taylor_test(Jhat, ic_param, Jvalue, dJdic)
+
