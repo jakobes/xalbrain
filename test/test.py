58a86e6a03dab6a2f646d0261e9721b16081cbfd
Marie E. Rognes <meg@simula.no>
2012-09-19 14:20:46 +0200

    Shuffle tests around.

--- test/test.py
+++ test/test.py
@@ -1,29 +1,41 @@
+"""
+Main driver script for running tests: runs all .py scripts in
+specified subdirectories
+"""
+
 import os
 import sys
 import instant
 
-tests = ["simple", "test_bumps", "analytic_bidomain", "cellmodeltest"]
-
-failed = []
+testdirectories = ["no-adjoints"]#, "adjoints"]
 
 # Command to run
-command = "python %s.py " + " ".join(sys.argv[1:])
+command = "python %s " + " ".join(sys.argv[1:])
 
+# Don't plot
 os.environ["DOLFIN_NOPLOT"] = "1"
 
-for test in tests:
-    if not os.path.isfile(os.path.join("%s.py" % test)):
-        continue
-    print "Running tests: %s" % test
-    print "----------------------------------------------------------------------"
-    fail, output = instant.get_status_output(command % test)
-    if fail:
-        print "failed"
-        failed.append(test)
-    else:
-        print "OK"
+# Walk through all Python files in given subdirectories and run each
+# script
+failed = []
+for test in testdirectories:
+    for subdir, dirs, files in os.walk(os.path.join(os.getcwd(), test)):
+        pyfiles = [filename for filename in files
+                   if (os.path.splitext(filename)[1] == ".py")]
 
-    print ""
+        for pyfile in pyfiles:
+            print "Running test: %s" % os.path.join(pyfile)
+            print "-"*80
+            absfile = os.path.join(subdir, pyfile)
+            fail, output = instant.get_status_output(command % absfile)
+
+            if fail:
+                failed.append(pyfile)
+                print "failed"
+                print output
+            else:
+                print "OK"
+            print ""
 
 if failed:
     print "Failed:", ", ".join(failed)
