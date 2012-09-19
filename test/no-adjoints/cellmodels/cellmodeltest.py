2a579fdd9dcd1acfa899b84f43e8c49245e03717
Marie E. Rognes <meg@simula.no>
2012-09-19 14:36:06 +0200

    Improve cell model test

--- test/no-adjoints/cellmodels/cellmodeltest.py
+++ test/no-adjoints/cellmodels/cellmodeltest.py
@@ -1,7 +1,14 @@
+"""
+Regression and correctness test for FitzHughNagumo model and pure
+CellSolver: compare (in eyenorm) time evolution with results from
+Section 2.4.1 p. 36 in Sundnes et al, 2006 (checked 2012-09-19), and
+check that maximal v/s values do not regress
+"""
+
+# Marie E. Rognes <meg@simula.no>
+# Last changed: 2012-09-19
+
 from dolfin import *
-import pylab
-import os
-#from dolfin_adjoint import *
 
 # Cardiac solver specific imports
 from beatadjoint import *
@@ -19,7 +26,6 @@ class AppliedCurrent(Expression):
 
 cell = FitzHughNagumo()
 cell.applied_current = AppliedCurrent()
-
 solver = CellSolver(cell)
 
 # Setup initial condition
@@ -36,12 +42,11 @@ times = []
 v_values = []
 s_values = []
 
+# Solve
 while (t1 <= T):
-    # Solve
     info_blue("Solving on t = (%g, %g)" % (t0, t1))
     timestep = (t0, t1)
     times += [(t0 + t1)/2]
-
     tmp = solver.step(timestep, vs_)
     vs.assign(tmp)
 
@@ -52,7 +57,20 @@ while (t1 <= T):
     vs_.assign(vs)
     t0 = t1; t1 = t0 + dt
 
+# Regression test
+v_max_reference = 2.3839115023509514e+01
+s_max_reference = 6.9925836316850834e+01
+tolerance = 1.e-14
+msg = "Maximal %s value does not match reference: diff is %.16e"
+v_diff = abs(max(v_values) - v_max_reference)
+s_diff = abs(max(s_values) - s_max_reference)
+assert (v_diff < tolerance), msg % ("v", v_diff)
+assert (s_diff < tolerance), msg % ("s", s_diff)
+
+# Correctness test
+import os
 if int(os.environ.get("DOLFIN_NOPLOT", 0)) != 1:
+    import pylab
     pylab.plot(times, v_values, 'b*')
     pylab.plot(times, s_values, 'r-')
     pylab.show()
