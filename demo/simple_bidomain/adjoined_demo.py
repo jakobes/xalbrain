139de4dc3d756ec178a29eb5df7796b02b60f862
Marie E. Rognes <meg@simula.no>
2012-10-03 16:33:20 +0200

    Some clean-ups in adjoint demo, declaring that this is working

--- demo/simple_bidomain/adjoined_demo.py
+++ demo/simple_bidomain/adjoined_demo.py
@@ -60,30 +60,26 @@ heart = MyHeart(cell)
 
 # Set-up solver
 ps = SplittingSolver.default_parameters()
-ps["linear_variational_solver"]["linear_solver"] = "direct"
-ps["nonlinear_variational_solver"]["linear_solver"] = "direct"
 ps["enable_adjoint"] = True
-#ps["theta"] = 0.5
 solver = SplittingSolver(heart, parameters=ps)
 
 # Define end-time and (constant) timestep
-k_n = 0.25 # mS
-T = 4.0  # mS
+T = 2.0
+k_n = 0.25
 
+# Split solve out into separate function for Taylor test purposes
 def main(ic):
-
     # Assign initial condition
     (vs_, vs, u) = solver.solution_fields()
-    vs_.adj_name = "VS_"
-    vs.adj_name = "VS"
-    u.adj_name = "U"
+    vs_.adj_name = "vs_"
+    vs.adj_name = "vs"
+    u.adj_name = "u"
     vs_.assign(ic, annotate=True)
 
     # Solve
     begin("Solving primal")
     solutions = solver.solve((0, T), k_n)
     for (timestep, vs, u) in solutions:
-        #plot(u)
         continue
     end()
 
@@ -91,42 +87,40 @@ def main(ic):
 
 if __name__ == "__main__":
 
+    # Define initial condition here (no need to annotate this step)
     ic = InitialCondition()
     ic = Function(project(ic, solver.VS, annotate=False))
 
-    # Run stuff
+    # Run forward simulation once
     (vs, u) = main(ic)
 
+    # Stop annotating here (done with forward solve)
     parameters["adjoint"]["stop_annotating"] = True
 
-    # 1: Compute value of functional at "end"
-    J_value = assemble(inner(vs, vs)*dx)   # Value of functional (last u)
-    print "J_value = ", J_value
+    # Compute value of functional at "end"
+    J_value = assemble(inner(vs, vs)*dx)
 
     # Check replay
     info_green("Replaying")
     success = replay_dolfin(tol=0.0, stop=True)
 
+    # Store record
     adj_html("forward.html", "forward")
     adj_html("adjoint.html", "adjoint")
 
-    # Define the amount of functionals needed
-    def M(vs):
-        return inner(vs, vs)*dx*dt[FINISH_TIME]
-    J = Functional(M(vs))  # Functional as dolfin_adjoint.Functional
+    # Define some functional
+    J = Functional(inner(vs, vs)*dx*dt[FINISH_TIME])
 
+    # Compute gradient of J with respect to vs_
     info_green("Computing gradient")
-    dJdic = compute_gradient(J, InitialConditionParameter("VS_"), forget=False)
+    dJdic = compute_gradient(J, InitialConditionParameter("vs_"), forget=False)
     assert dJdic is not None
 
+    # Run Taylor test
     info_green("Verifying")
-    def Jhat(ic):           # Functional value as function of ic
+    def Jhat(ic):
         (vs, u) = main(ic)
         return assemble(inner(vs, vs)*dx)
-
-    # Look at some Taylor test
-    minconv = taylor_test(Jhat, InitialConditionParameter("VS_"),
-                          J_value, dJdic)
-
+    minconv = taylor_test(Jhat, InitialConditionParameter("vs_"),J_value, dJdic)
     print "Minimum convergence rate: ", minconv
 
