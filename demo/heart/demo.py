31546bc399273ae2f127f4ee62f0aad7f18c265b
Marie E. Rognes <meg@simula.no>
2012-10-22 17:06:22 +0200

    Use tenTusscher model -- matrix assembly takes forever (even with only quad degree 2, must turn off form_optimize b/c infinite recursion issues) Hm... Must think.

--- demo/heart/demo.py
+++ demo/heart/demo.py
@@ -10,8 +10,11 @@ from dolfin_adjoint import *
 from beatadjoint import *
 
 parameters["reorder_dofs"] = False
+parameters["form_compiler"]["quadrature_degree"] = 2
 parameters["form_compiler"]["cpp_optimize"] = True
-parameters["form_compiler"]["optimize"] = True
+#parameters["form_compiler"]["optimize"] = True
+
+set_log_level(PROGRESS)
 
 class SampleConductivity(Expression):
     """Randomly chosen values by Marie to represent some spatially
@@ -55,29 +58,12 @@ class MyHeart(CardiacModel):
         s_el = 2.0/chi
         s_et = 1.3/chi
         s_ez = 1.3/chi
-
         M_i = as_tensor(((s_il, 0, 0), (0, s_it, 0), (0, 0, s_iz)))
         M_e = as_tensor(((s_el, 0, 0), (0, s_et, 0), (0, 0, s_ez)))
         return (M_i, M_e)
 
-def cell_model():
-    # Set-up cell model
-    k = 0.00004;
-    Vrest = -85.;
-    Vthreshold = -70.;
-    Vpeak = 40.;
-    k = 0.00004;
-    l = 0.63;
-    b = 0.013;
-    v_amp = Vpeak - Vrest
-    cell_parameters = {"c_1": k*v_amp**2, "c_2": k*v_amp, "c_3": b/l,
-                       "a": (Vthreshold - Vrest)/v_amp, "b": l,
-                       "v_rest":Vrest, "v_peak": Vpeak}
-    model = FitzHughNagumo(cell_parameters)
-    return model
-
 # Setup cell and cardiac model
-cell = cell_model()
+cell = Tentusscher_2004_mcell()
 heart = MyHeart(cell)
 
 # Set-up solver
@@ -88,11 +74,10 @@ ps["linear_variational_solver"]["linear_solver"] = "direct"
 solver = Solver(heart, parameters=ps)
 
 # Define initial condition here (no need to annotate this step)
-ic0 = InitialCondition()
-ic = Function(project(ic0, solver.VS, annotate=False))
+ic = project(cell.initial_conditions(), solver.VS, annotate=False)
 
 # Define end-time and (constant) timestep
-T = 100.0
+T = 0.25
 k_n = 0.25
 
 # Assign initial condition
