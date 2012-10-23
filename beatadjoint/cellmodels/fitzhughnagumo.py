e30576e85a2e91333b32882e528cd8654c7c968f
Johan Hake <hake.dev@gmail.com>
2012-10-23 11:49:26 +0200

    Added Gotran model for FHN   -- Due to symbolic treatment of expressions it differ from the      original, hope just works :)

--- beatadjoint/cellmodels/fitzhughnagumo.py
+++ beatadjoint/cellmodels/fitzhughnagumo.py
@@ -1,62 +1,87 @@
-"""This module contains a FitzHugh-Nagumo cardiac cell models."""
 
-# Copyright (C) 2012 Marie E. Rognes (meg@simula.no)
-# Use and modify at will
-# Last changed: 2012-10-22
+import ufl
 
-__all__ = ["FitzHughNagumo"]
+from dolfin import *
+from dolfin_adjoint import *
 
-from dolfin import Parameters
-from cardiaccellmodel import *
+from beatadjoint.cellmodels import CardiacCellModel
 
-# ------------------------------------------------------------------------------
-# FitzHughNagumo model
-# ------------------------------------------------------------------------------
-class FitzHughNagumo(CardiacCellModel):
+class Fitzhughnagumo(CardiacCellModel):
     """
-    Reparametrized FitzHughNagumo model
-    (cf. 2.4.1 in Sundnes et al 2006)
+NOT_IMPLEMENTED    
     """
     def __init__(self, parameters=None):
         CardiacCellModel.__init__(self, parameters)
 
     def default_parameters(self):
-        parameters = Parameters("FitzHughNagumo")
+        parameters = Parameters("Fitzhughnagumo")
         parameters.add("a", 0.13)
         parameters.add("b", 0.013)
         parameters.add("c_1", 0.26)
         parameters.add("c_2", 0.1)
         parameters.add("c_3", 1.0)
-        parameters.add("v_rest", -85.)
-        parameters.add("v_peak", 40.)
+        parameters.add("v_peak", 40.0)
+        parameters.add("v_rest", -85.0)
         return parameters
 
     def I(self, v, s):
-        # Extract parameters
+        """
+        Transmembrane current
+        """
+        # Imports
+        # No imports for now
+
+        # Assign states
+        states = s
+        assert(len(states) == 1)
+        s, = states
+
+        # Assign parameters
+        a = self._parameters["a"]
         c_1 = self._parameters["c_1"]
         c_2 = self._parameters["c_2"]
         v_rest = self._parameters["v_rest"]
         v_peak = self._parameters["v_peak"]
-        v_amp = v_peak - v_rest
-        v_th = v_rest + self._parameters["a"]*v_amp
 
-        # Define current
-        i = (c_1/(v_amp**2)*(v - v_rest)*(v - v_th)*(v_peak - v)
-             - c_2/(v_amp)*(v - v_rest)*s)
-        return - i
+        current = -c_1*(-v + v_peak)*(v - v_rest)*(-a*(v_peak - v_rest) + v -\
+            v_rest)/(v_peak - v_rest*v_peak - v_rest) + c_2*s*(v -\
+            v_rest)/(v_peak - v_rest)
+
+        
+        return current
 
     def F(self, v, s):
-        # Extract parameters
+        """
+        Right hand side for ODE system
+        """
+        # Imports
+        # No imports for now
+
+        # Assign states
+        states = s
+        assert(len(states) == 1)
+        s, = states
+
+        # Assign parameters
+        c_3 = self._parameters["c_3"]
         b = self._parameters["b"]
         v_rest = self._parameters["v_rest"]
-        c_3 = self._parameters["c_3"]
 
-        # Define model
-        return b*(v - v_rest - c_3*s)
+        F_expressions = [\
+
+            b*(-c_3*s + v - v_rest),
+            ]
+
+        return as_vector(F_expressions)
+
+    def initial_conditions(self):
+        ic = Expression(["v", "s"],\
+            v=-85.0, s = 0.0)
+
+        return ic
 
     def num_states(self):
         return 1
 
     def __str__(self):
-        return "FitzHugh-Nagumo cardiac cell model"
-
+        return 'Fitzhughnagumo cardiac cell model'
