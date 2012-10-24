40c27a2269a3d18df753d937410bf9f651fb5737
Marie E. Rognes <meg@simula.no>
2012-10-24 11:02:51 +0200

    Update repo and demo with data from Johan and Molly. Thanks!
    "Missing" parameters:
    
       chi: (using 2000 1/cm from book)
       C_m: (using 1.0 mu F /(cm^2) from book)
    
       T:   (end time = ? )
       k_n: (suitable time step = ?)
    
    Problem 1: Newton solver suddenly stopped converting (with no input
    and starting at resting state...) Hm...!

--- demo/heart/demo.py
+++ demo/heart/demo.py
@@ -2,47 +2,97 @@
 """
 
 # Marie E. Rognes <meg@simula.no>
-# Last changed: 2012-10-23
+# Last changed: 2012-10-24
 
 import math
 from dolfin import *
 from dolfin_adjoint import *
 from beatadjoint import *
-
-#parameters["reorder_dofs"] = False
-parameters["form_compiler"]["cpp_optimize"] = True
-parameters["form_compiler"]["optimize"] = False
+import time
 
 set_log_level(PROGRESS)
 
-# FIXMEs:
-chi = 2000.0   # Membrane surface-to-volume ratio (Need value + unit)
-C_m = 1.0      # Membrane capacitance per unit area (Need value + unit)
+parameters["reorder_dofs"] = False # Crucial!
+parameters["form_compiler"]["cpp_optimize"] = True
+parameters["form_compiler"]["optimize"] = True
+
+# Generic cardiac parameters
+chi = 2000.0   # Membrane surface-to-volume ratio (1/cm), value from book
+# NB : If not 1 => must scale ionic current! see book p. 55
+C_m = 1.0      # Membrane capacitance per unit area (micro F/(cm^2))
+
+# Domain
+# FIXME: MER: Check that no cell markers are in here:
+mesh = Mesh("data/mesh115_refined.xml.gz")
+mesh.coordinates()[:] /= 4 # Scale mesh as indicated by Johan
+
+print "Coordinates (min, max), x, y, z:"
+print [min(mesh.coordinates()[:, 0]), max(mesh.coordinates()[:, 0])]
+print [min(mesh.coordinates()[:, 1]), max(mesh.coordinates()[:, 1])]
+print [min(mesh.coordinates()[:, 2]), max(mesh.coordinates()[:, 2])]
+#exit()
+
+# Time and time-step
 T = 1.0        # End time (need value + unit)
 k_n = 0.25     # Timestep (need value)
 
-# FIXME: Define some 'stimulation protocol': Johan will fix
-class Pulse(Expression):
-    def eval(self, values, x):
-        values[0] = 0.0
-        if (x[0] > 59.0):
-            values[0] = 1.0
+# Load fibers and sheets
+Vv = VectorFunctionSpace(mesh, "DG", 0)
+fiber = Function(Vv)
+File("data/fibers.xml.gz") >> fiber
+sheet = Function(Vv)
+File("data/sheet.xml.gz") >> sheet
+cross_sheet = Function(Vv)
+File("data/cross_sheet.xml.gz") >> cross_sheet
 
-# FIXME: Get finer mesh and fibers from Molly/Sjur/Johan
-mesh = Mesh("../../data/meshes/mesh115_coarse_no_markers.xml.gz")
+# Load ischemic region (scalar function between 0-1, where 0 is ischemic)
+V = FunctionSpace(mesh, "CG", 1)
+ischemic = Function(V)
+File("data/ischemic_region.xml.gz") >> ischemic
+ischemic_array = ischemic.vector().array()
 
-# FIXME: All of the conductivities need specification in healthy and
-# ischemic tissue (Johan and Molly will give to marie Marie)
-class SampleConductivity(Expression):
-    """Randomly chosen values by Marie to represent some spatially
-    varying conductivity"""
-    def eval(self, values, x):
-        values[0] = 3.0/(chi*C_m)
+# Healthy and ischemic conductivities
+# (All values in mS/cm (milli-Siemens per centimeter)
 
-# This will be changed
-m = SampleConductivity()
-CG1 = FunctionSpace(mesh, "CG", 1)
-m = Function(interpolate(m, CG1, annotate=False), name="CellConductivity")
+# Extracellular:
+g_el = 6.25/(C_m*chi) # Fiber
+g_et = 2.36/(C_m*chi) # Sheet
+g_et = 2.36/(C_m*chi) # Cross-sheet
+
+# Intracellular:
+g_il = 1.74/(C_m*chi)   # Fiber
+g_it = 0.192/(C_m*chi)  # Sheet
+g_it = 0.192/(C_m*chi)  # Cross-sheet
+
+# Extracellular:
+g_el_isch = 3.125/(C_m*chi) # Fiber
+g_et_isch = 1.18/(C_m*chi) # Sheet
+g_et_isch = 1.18/(C_m*chi) # Cross-sheet
+
+# Intracellular:
+g_il_isch = 0.125/(C_m*chi)  # Fiber
+g_it_isch = 0.125/(C_m*chi)  # Sheet
+g_it_isch = 0.125/(C_m*chi)  # Cross-sheet
+
+# Combine info into 2x2 distinct conductivity functions:
+g_el_field = Function(V)
+g_el_field.vector()[:] = (1-ischemic_array)*g_el_isch+ischemic_array*g_el
+g_et_field = Function(V)
+g_et_field.vector()[:] = (1-ischemic_array)*g_et_isch+ischemic_array*g_et
+g_il_field = Function(V)
+g_il_field.vector()[:] = (1-ischemic_array)*g_il_isch+ischemic_array*g_il
+g_it_field = Function(V)
+g_it_field.vector()[:] = (1-ischemic_array)*g_it_isch+ischemic_array*g_it
+
+# Construct conductivity tensors from directions and conductivity
+# values relative to that coordinate system
+A = as_matrix([[fiber[0], sheet[0], cross_sheet[0]],
+               [fiber[1], sheet[1], cross_sheet[1]],
+               [fiber[2], sheet[2], cross_sheet[2]]])
+M_e_star = diag(as_vector([g_el_field, g_et_field, g_et_field]))
+M_i_star = diag(as_vector([g_il_field, g_it_field, g_it_field]))
+M_e = A*M_e_star*A.T
+M_i = A*M_i_star*A.T
 
 class MyHeart(CardiacModel):
     def __init__(self, cell_model):
@@ -50,21 +100,17 @@ class MyHeart(CardiacModel):
     def domain(self):
         return mesh
     def conductivities(self):
-        # FIXME cf comments above
-        s_il = m
-        s_it = 0.3/(chi*C_m)
-        s_iz = 2.0/(chi*C_m)
-        s_el = 2.0/(chi*C_m)
-        s_et = 1.3/(chi*C_m)
-        s_ez = 1.3/(chi*C_m)
-
-        M_i = as_tensor(((s_il, 0, 0), (0, s_it, 0), (0, 0, s_iz)))
-        M_e = as_tensor(((s_el, 0, 0), (0, s_et, 0), (0, 0, s_ez)))
         return (M_i, M_e)
 
 # Setup cell and cardiac model
 cell = OriginalFitzHughNagumo()
 heart = MyHeart(cell)
+
+# FIXME: Define some 'stimulation protocol': Johan will fix
+stimulation_cells = MeshFunction("uint", mesh, "data/stimulation_cells.xml.gz")
+class Pulse(Expression):
+    def eval(self, values, x):
+        values[0] = 0.0
 
 # Add given current as Stimulus
 heart.stimulus = Pulse()
@@ -86,17 +132,27 @@ vs.adj_name = "vs"
 u.adj_name = "u"
 vs_.assign(ic, annotate=True)
 
+(v_, s_) = vs_.split(deepcopy=True)
+plot(v_, title="v_")
+plot(s_, title="s_")
+interactive()
+
 # Solve
 begin("Solving primal")
+start = time.time()
 solutions = solver.solve((0, T), k_n)
 for (timestep, vs, u) in solutions:
-    plot(u, title="Extracellular potential (u)")
+    #plot(u, title="Extracellular potential (u)")
     continue
+stop = time.time()
+print "Time elapsed: %g" % (stop - start)
 end()
 
+list_timings()
+
 (v, s) = split(vs)
-plot(v, title="Transmembrane potential (v)")
-interactive()
+#plot(v, title="Transmembrane potential (v)")
+#interactive()
 
 # # Define some functional
 J = Functional(inner(v, v)*ds*dt)
--- demo/heart/demo2d.py
+++ demo/heart/demo2d.py
@@ -18,15 +18,17 @@ set_log_level(PROGRESS)
 # FIXMEs:
 chi = 2000.0   # Membrane surface-to-volume ratio (Need value + unit)
 C_m = 1.0      # Membrane capacitance per unit area (Need value + unit)
-T = 100.0        # End time (need value + unit)
+T = 5.0        # End time (need value + unit)
 k_n = 0.25     # Timestep (need value)
+
+factor = 10.
 
 # FIXME: Define some 'stimulation protocol': Johan will fix
 class Pulse(Expression):
     def eval(self, values, x):
         values[0] = 0.0
-        if (x[0] > 0.8):
-            values[0] = 1.0
+        if (x[0] > 0.9):
+            values[0] = factor*1.0
 
 # FIXME: Get finer mesh and fibers from Molly/Sjur/Johan
 #mesh = Mesh("../../data/meshes/mesh115_coarse_no_markers.xml.gz")
@@ -38,12 +40,18 @@ class SampleConductivity(Expression):
     """Randomly chosen values by Marie to represent some spatially
     varying conductivity"""
     def eval(self, values, x):
-        values[0] = 3.0/(chi*C_m)
+        origo = (0.8, 0.5)
+        r = 0.1
+        if ((x[0] - origo[0])**2 + (x[1] - origo[1])**2 < r**2):
+            values[0] = factor*3.0/(chi*C_m)
+        else:
+            values[0] = factor**2*3.0/(chi*C_m)
 
 # This will be changed
 m = SampleConductivity()
 CG1 = FunctionSpace(mesh, "CG", 1)
 m = Function(interpolate(m, CG1, annotate=False), name="CellConductivity")
+plot(m, interactive=True)
 
 class MyHeart(CardiacModel):
     def __init__(self, cell_model):
@@ -53,9 +61,9 @@ class MyHeart(CardiacModel):
     def conductivities(self):
         # FIXME cf comments above
         s_il = m
-        s_it = 0.3/(chi*C_m)
-        s_el = 2.0/(chi*C_m)
-        s_et = 1.3/(chi*C_m)
+        s_it = factor*0.3/(chi*C_m)
+        s_el = factor*2.0/(chi*C_m)
+        s_et = factor*1.3/(chi*C_m)
 
         M_i = as_tensor(((s_il, 0), (0, s_it)))
         M_e = as_tensor(((s_el, 0), (0, s_et)))
@@ -90,13 +98,16 @@ vs_.assign(ic, annotate=True)
 begin("Solving primal")
 solutions = solver.solve((0, T), k_n)
 file = File("results-2d/u.pvd")
+v_plot = Function(solver.VS.sub(0).collapse())
 for (timestep, vs, u) in solutions:
+    v_plot.assign(vs.split(deepcopy=True)[0], annotate=False)
+    plot(v_plot, title="Membrane potential (v)")
     #plot(u, title="Extracellular potential (u)")
-    file << u
+    #file << u
 end()
 
 (v, s) = split(vs)
-plot(v, title="Transmembrane potential (v)")
+plot(v_plot, title="Transmembrane potential (v)")
 interactive()
 
 # # Define some functional
