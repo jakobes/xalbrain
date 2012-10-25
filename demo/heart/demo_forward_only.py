e0b0f3d509b617a763b1ef28792f39868897d6bf
Marie E. Rognes <meg@simula.no>
2012-10-25 13:06:49 +0200

    Some clean-ups and checks.

--- demo/heart/demo_forward_only.py
+++ demo/heart/demo_forward_only.py
@@ -8,6 +8,7 @@ import math
 from dolfin import *
 from dolfin_adjoint import *
 from beatadjoint import *
+
 import time
 
 set_log_level(PROGRESS)
@@ -16,18 +17,23 @@ set_log_level(PROGRESS)
 application_parameters = Parameters("Application")
 application_parameters.add("T", 0.5)         # End time  (ms)
 application_parameters.add("timestep", 0.05) # Time step (ms)
-application_parameters.add("directory", "default-results") # Time step (ms)
+application_parameters.add("directory", "default-results")
+application_parameters.add("backend", "PETSc")
 application_parameters.parse()
 info(application_parameters, True)
 
+# Update backend from application parameters
+parameters["linear_algebra_backend"] = application_parameters["backend"]
+
+# Adjust some general parameters
 parameters["reorder_dofs"] = False # Crucial!
 parameters["form_compiler"]["cpp_optimize"] = True
 parameters["form_compiler"]["optimize"] = True
 
 # Generic cardiac parameters
-chi = 2000.0   # Membrane surface-to-volume ratio (1/cm), value from book
+#chi = 2000.0   # Membrane surface-to-volume ratio (1/cm), value from book
 # NB : If not 1 => must scale ionic current! see book p. 55
-C_m = 1.0      # Membrane capacitance per unit area (micro F/(cm^2))
+#C_m = 1.0      # Membrane capacitance per unit area (micro F/(cm^2))
 
 # Domain
 mesh = Mesh("data/mesh115_refined.xml.gz")
@@ -47,44 +53,12 @@ File("data/sheet.xml.gz") >> sheet
 cross_sheet = Function(Vv)
 File("data/cross_sheet.xml.gz") >> cross_sheet
 
-# Load ischemic region (scalar function between 0-1, where 0 is ischemic)
+# Extract conducitivity data
 V = FunctionSpace(mesh, "CG", 1)
-ischemic = Function(V)
-File("data/ischemic_region.xml.gz") >> ischemic
-ischemic_array = ischemic.vector().array()
-
-# Healthy and ischemic conductivities
-# (All values in mS/cm (milli-Siemens per centimeter)
-
-# Extracellular:
-g_el = 6.25/(C_m*chi) # Fiber
-g_et = 2.36/(C_m*chi) # Sheet
-g_et = 2.36/(C_m*chi) # Cross-sheet
-
-# Intracellular:
-g_il = 1.74/(C_m*chi)   # Fiber
-g_it = 0.192/(C_m*chi)  # Sheet
-g_it = 0.192/(C_m*chi)  # Cross-sheet
-
-# Extracellular:
-g_el_isch = 3.125/(C_m*chi) # Fiber
-g_et_isch = 1.18/(C_m*chi) # Sheet
-g_et_isch = 1.18/(C_m*chi) # Cross-sheet
-
-# Intracellular:
-g_il_isch = 0.125/(C_m*chi)  # Fiber
-g_it_isch = 0.125/(C_m*chi)  # Sheet
-g_it_isch = 0.125/(C_m*chi)  # Cross-sheet
-
-# Combine info into 2x2 distinct conductivity functions:
-g_el_field = Function(V)
-g_el_field.vector()[:] = (1-ischemic_array)*g_el_isch+ischemic_array*g_el
-g_et_field = Function(V)
-g_et_field.vector()[:] = (1-ischemic_array)*g_et_isch+ischemic_array*g_et
-g_il_field = Function(V)
-g_il_field.vector()[:] = (1-ischemic_array)*g_il_isch+ischemic_array*g_il
-g_it_field = Function(V)
-g_it_field.vector()[:] = (1-ischemic_array)*g_it_isch+ischemic_array*g_it
+g_el_field = Function(V, "data/g_el_field.xml.gz")
+g_et_field = Function(V, "data/g_et_field.xml.gz")
+g_il_field = Function(V, "data/g_il_field.xml.gz")
+g_it_field = Function(V, "data/g_it_field.xml.gz")
 
 # Construct conductivity tensors from directions and conductivity
 # values relative to that coordinate system
@@ -110,61 +84,29 @@ heart = MyHeart(cell)
 
 # Define some simulation protocol (use cpp expression for speed)
 stimulation_cells = MeshFunction("uint", mesh, "data/stimulation_cells.xml.gz")
-cpp_stimulus = """
-class Stimulus : public Expression
-{
-public:
-
-  boost::shared_ptr<MeshFunction<unsigned int> > cell_data;
-
-  Stimulus() : Expression(), amplitude(0), duration(0), t(0)
-  {
-  }
-
-  void eval(Array<double>& values, const Array<double>& x,
-            const ufc::cell& c) const
-  {
-    assert(cell_data);
-    const Cell cell(cell_data->mesh(), c.index);
-    switch ((*cell_data)[cell.index()])
-    {
-    case 0:
-      values[0] = 0.0;
-      break;
-    case 1:
-      if (t <= duration)
-        values[0] = amplitude;
-      else
-        values[0] = 0.0;
-      break;
-    default:
-      values[0] = 0.0;
-    }
-  }
-  double amplitude;
-  double duration;
-  double t;
-};"""
-
-# Define pulse stimulus
+from stimulation import cpp_stimulus
 pulse = Expression(cpp_stimulus)
 pulse.cell_data = stimulation_cells
 pulse.amplitude = 7.0 # Ampere
 pulse.duration = 10.0 # ms
 pulse.t = 0.0         # ms
 
-# Assign pulse as stimulus to heart
 heart.stimulus = pulse
 
 # Set-up solver
+begin("Setting-up solver")
 Solver = SplittingSolver
 ps = Solver.default_parameters()
+ps["default_timestep"] = k_n
 ps["enable_adjoint"] = True
 ps["linear_variational_solver"]["linear_solver"] = "direct"
 solver = Solver(heart, parameters=ps)
+end()
 
 # Define initial condition here (no need to annotate this step)
+begin("Projecting initial condition")
 ic = project(cell.initial_conditions(), solver.VS, annotate=False)
+end()
 
 # Assign initial condition
 (vs_, vs, u) = solver.solution_fields()
@@ -180,6 +122,11 @@ solutions = solver.solve((0, T), k_n)
 
 timestep_counter = 1
 directory = application_parameters["directory"]
+
+# Store application parameters
+parametersfile = File("%s/parameters.xml" % directory)
+parametersfile << application_parameters
+# (Compute) and store solutions
 for (timestep, vs, u) in solutions:
     vsfile = File("%s/vs_%d.xml.gz" % (directory, timestep_counter))
     vsfile << vs
---
+++ demo/heart/plot_results.py
@@ -0,0 +1,33 @@
+from dolfin import *
+
+parameters["reorder_dofs"] = False # Crucial!
+parameters["form_compiler"]["cpp_optimize"] = True
+parameters["form_compiler"]["optimize"] = True
+
+mesh = Mesh("data/mesh115_refined.xml.gz")
+mesh.coordinates()[:] /= 1000.0 # Scale mesh from micrometer to millimeter
+mesh.coordinates()[:] /= 4.0    # Scale mesh as indicated by Johan
+
+V = FunctionSpace(mesh, "CG", 1)
+S = FunctionSpace(mesh, "DG", 0)
+W = V*S
+
+directory = "default-results"
+
+u_plot = Function(V)
+v_plot = Function(W.sub(0).collapse())
+s_plot = Function(W.sub(1).collapse())
+for i in range(1, 10):
+    print "i = ", i
+    vs = Function(W, "%s/vs_%d.xml.gz" % (directory, i))
+    u = Function(V, "%s/u_%d.xml.gz" % (directory, i))
+    (v, s) = vs.split(deepcopy=True)
+    u_plot.assign(u)
+    v_plot.assign(v)
+    s_plot.assign(s)
+
+    plot(u_plot, title="u")
+    plot(v_plot, title="v")
+    plot(s_plot, title="s")
+
+interactive()
