05efe2e8b5b77e4f9953ed9de07bad4de53d99ef
Marie E. Rognes <meg@simula.no>
2013-05-13 13:26:13 +0200

    Add replay test for adjoints of bidomain solvers. Using direct solvers for now, awaiting nullspace support in dolfin-adjoint...

--- test/unit/test_bidomain_adjoints.py
+++ test/unit/test_bidomain_adjoints.py
@@ -3,8 +3,8 @@ Unit tests for various types of solvers for cardiac cell models.
 """
 
 __author__ = "Marie E. Rognes (meg@simula.no), 2013"
-__all__ = ["TestBasicSingleCellSolverAdjoint"]
-
+__all__ = ["TestBasicBidomainSolverAdjoint",
+           "TestBidomainSolverAdjoint"]
 
 import unittest
 from dolfin import *
@@ -34,6 +34,7 @@ class TestBasicBidomainSolverAdjoint(unittest.TestCase):
     "Test adjoint functionality for the basic bidomain solver."
 
     def setUp(self):
+        adj_reset()
         self.case = TestCase()
 
     def test_replay(self):
@@ -42,9 +43,13 @@ class TestBasicBidomainSolverAdjoint(unittest.TestCase):
         case = self.case
 
         # Create solver
+        params = Solver.default_parameters()
+        params["linear_variational_solver"]["linear_solver"] = "lu"
+
         solver = Solver(case.mesh, case.time, case.M_i, case.M_e,
                         I_s=case.stimulus,
-                        I_a=case.applied_current)
+                        I_a=case.applied_current,
+                        params=params)
 
         # Solve
         info_green("Running forward model")
@@ -59,12 +64,37 @@ class TestBasicBidomainSolverAdjoint(unittest.TestCase):
 
 class TestBidomainSolverAdjoint(unittest.TestCase):
     "Test adjoint functionality for the bidomain solver."
+
+    def setUp(self):
+        adj_reset()
+        self.case = TestCase()
+
     def test_replay(self):
-        pass
+        "Test that replay of basic bidomain solver reports success."
+        Solver = BidomainSolver
+        case = self.case
+
+        # Create solver
+        params = Solver.default_parameters()
+        params["linear_solver_type"] = "direct"
+        solver = Solver(case.mesh, case.time, case.M_i, case.M_e,
+                        I_s=case.stimulus,
+                        I_a=case.applied_current, params=params)
+
+        # Solve
+        info_green("Running forward model")
+        solutions = solver.solve((case.t0, case.t0 + case.T), case.dt)
+        for (interval, fields) in solutions:
+            (v_, vs) = fields
+
+        # Check replay
+        info_green("Running replay")
+        success = replay_dolfin(stop=True, tol=0.0)
+        self.assertEqual(success, True)
 
 
 if __name__ == "__main__":
     print("")
     print("Testing adjoints of bidomain solvers")
-    print("--------------------------------")
+    print("------------------------------------")
     unittest.main()
