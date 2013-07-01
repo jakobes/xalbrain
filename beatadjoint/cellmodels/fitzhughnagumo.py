234d529ba88a9e93287b31a69f7dc51e74021620
Johan Hake <hake.dev@gmail.com>
2013-07-01 09:43:39 +0200

    Fix bug in gotran generated fitzhughnagumo model.

--- beatadjoint/cellmodels/fitzhughnagumo.py
+++ beatadjoint/cellmodels/fitzhughnagumo.py
@@ -36,6 +36,10 @@ NOT_IMPLEMENTED
                               ("c_1", 0.26),
                               ("c_2", 0.1),
                               ("c_3", 1.0),
+                              ("stim_amplitude", 0),
+                              ("stim_duration", 1),
+                              ("stim_period", 1000),
+                              ("stim_start", 1),
                               ("v_peak", 40.0),
                               ("v_rest", -85.0)])
         return params
@@ -60,14 +64,19 @@ NOT_IMPLEMENTED
 
         # Assign parameters
         a = self._parameters["a"]
+        stim_start = self._parameters["stim_start"]
+        stim_amplitude = self._parameters["stim_amplitude"]
         c_1 = self._parameters["c_1"]
         c_2 = self._parameters["c_2"]
         v_rest = self._parameters["v_rest"]
+        stim_duration = self._parameters["stim_duration"]
         v_peak = self._parameters["v_peak"]
 
-        current = -(v - v_rest)*c_2*s/(v_peak - v_rest) + (v_peak - v)*(v -\
-            v_rest)*(-(v_peak - v_rest)*a + v - v_rest)*c_1/((v_peak -\
-            v_rest)*(v_peak - v_rest))
+        current = -(v - v_rest)*(v_peak - v)*(-(v_peak - v_rest)*a + v -\
+            v_rest)*c_1/((v_peak - v_rest)*(v_peak - v_rest)) + (v -\
+            v_rest)*c_2*s/(v_peak - v_rest) - (1.0 - 1.0/(1.0 +\
+            ufl.exp(-5.0*stim_start + 5.0*time)))*stim_amplitude/(1.0 +\
+            ufl.exp(-5.0*stim_start + 5.0*time - 5.0*stim_duration))
 
         return current
 
@@ -90,7 +99,7 @@ NOT_IMPLEMENTED
         F_expressions = [\
 
             # Derivative for state s
-            (v - c_3*s - v_rest)*b,
+            (-c_3*s + v - v_rest)*b,
             ]
 
         return as_vector(F_expressions) if _is_vector else F_expressions[0]
