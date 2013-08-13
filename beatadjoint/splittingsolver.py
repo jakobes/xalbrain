fb13bd4ac08c9ae331b76ce927350f38bb35b4ad
Johan Hake <hake.dev@gmail.com>
2013-08-13 15:46:23 +0200

    Add limits to theta parameter

--- beatadjoint/splittingsolver.py
+++ beatadjoint/splittingsolver.py
@@ -205,7 +205,7 @@ class BasicSplittingSolver:
 
         params = Parameters("BasicSplittingSolver")
         params.add("enable_adjoint", True)
-        params.add("theta", 0.5)
+        params.add("theta", 0.5, 0, 1)
         params.add("apply_stimulus_current_to_pde", False)
         params.add("pde_solver", "bidomain", ["bidomain", "monodomain"])
 
@@ -397,7 +397,7 @@ class SplittingSolver(BasicSplittingSolver):
 
         params = Parameters("SplittingSolver")
         params.add("enable_adjoint", True)
-        params.add("theta", 0.5)
+        params.add("theta", 0.5, 0, 1)
         params.add("apply_stimulus_current_to_pde", False)
         params.add("pde_solver", "bidomain", ["bidomain", "monodomain"])
 
