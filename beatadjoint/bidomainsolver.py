ebf584f0a042b96597b8e7c1d9dc5ed10f052cbb
Johan Hake <hake.dev@gmail.com>
2013-11-21 14:17:56 +0100

    Fixes for changes in DOLFIN interface

--- beatadjoint/bidomainsolver.py
+++ beatadjoint/bidomainsolver.py
@@ -435,7 +435,7 @@ class BidomainSolver(BasicBidomainSolver):
         params["lu_solver"]["same_nonzero_pattern"] = True
 
         # Customize default parameters for KrylovSolver
-        params["krylov_solver"]["preconditioner"]["same_nonzero_pattern"] = True
+        params["krylov_solver"]["preconditioner"]["structure"] = "same"
 
         return params
 
