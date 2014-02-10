5a5e475b7cf1131be2753f243a395d957166e5be
Patrick Farrell <patrick.farrell@imperial.ac.uk>
2014-02-10 13:50:37 +0000

    Marie suggests I use BasicCardiacODESolver directly, instead.

--- beatadjoint/cellsolver.py
+++ beatadjoint/cellsolver.py
@@ -526,7 +526,7 @@ class BasicSingleCellSolver(BasicCardiacODESolver):
 
     """
 
-    def __init__(self, model, time, domain=None, params=None):
+    def __init__(self, model, time, params=None):
         "Create solver from given cell model and optional parameters."
 
         assert isinstance(model, CardiacCellModel), \
@@ -540,8 +540,7 @@ class BasicSingleCellSolver(BasicCardiacODESolver):
         self._model = model
 
         # Define carefully chosen dummy domain
-        if domain is None:
-          domain = UnitIntervalMesh(1)
+        domain = UnitIntervalMesh(1)
 
         # Extract information from cardiac cell model and ship off to
         # super-class.
