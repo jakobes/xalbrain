c89ddb55a6f9fedecb431752740e3921eb519b23
Simon Funke <simon.funke@gmailcom>
2014-02-11 17:07:34 +0100

    Split test_cellsolvers into test_cardiacodesolver and test_basicsinglecellsolver.

--- test/unit/test_cardiacodesolver.py
+++ test/unit/test_cardiacodesolver.py
@@ -9,7 +9,7 @@ import itertools
 import pytest
 from testutils import slow, assert_almost_equal, parametrize, cell_model
 
-from dolfin import info_red, info_green, \
+from dolfin import info, info_red, info_green, \
         UnitIntervalMesh, MPI, mpi_comm_world
 from beatadjoint import supported_cell_models, \
         CardiacODESolver, BasicSingleCellSolver, \
@@ -66,7 +66,7 @@ class TestCardiacODESolver(object):
         try:
             ind, ref_value = self.references[Model][Scheme]
         except KeyError:
-            info("Missing references for %s, %s: value is %g"
+            info_red("Missing references for %s, %s: value is %g"
                  % (Model, Scheme, sol[0]))
             return
 
@@ -145,69 +145,3 @@ class TestCardiacODESolver(object):
         solver.step((next_dt, 2*next_dt))
 
         self.compare_against_reference(vs.vector(), Model, Scheme)
-
-
-class TestBasicSingleCellSolver(object):
-    "Test functionality for the basic single cell solver."
-
-    references = {NoCellModel: {1.0: (0, 0.3),
-                                 0.5: (0, 0.2),
-                                 0.0: (0, 0.1)},
-                   FitzHughNagumoManual: {1.0:  (0, -84.70013280019053),
-                                          0.5: (0, -84.8000503072239979),
-                                          0.0:  (0, -84.9)},
-                   Tentusscher_2004_mcell: {1.0: (1, -85.89745525156506),
-                                            0.5: (1, -85.99686000794499),
-                                            0.0:  (1, -86.09643254164848),}
-                   }
-
-    def _run_solve(self, model, time, theta):
-        "Run two time steps for the given model with the given theta solver."
-        dt = 0.01
-        T = 2*dt
-        interval = (0.0, T)
-
-        # Initialize solver
-        params = BasicSingleCellSolver.default_parameters()
-        params["theta"] = theta
-
-        params["enable_adjoint"] = False
-        solver = BasicSingleCellSolver(model, time, params=params)
-
-        # Assign initial conditions
-        (vs_, vs) = solver.solution_fields()
-        vs_.assign(model.initial_conditions())
-
-        # Solve for a couple of steps
-        solutions = solver.solve(interval, dt)
-        for ((t0, t1), vs) in solutions:
-            pass
-
-        # Check that we are at the end time
-        assert_almost_equal(t1, T, 1e-10)
-        return vs.vector()
-
-    @slow
-    @parametrize(("theta"), [0., 0.5, 1.])
-    def test_default_basic_single_cell_solver(self, cell_model, theta):
-        "Test basic single cell solver."
-        time = Constant(0.0)
-        model = cell_model
-        Model = cell_model.__class__
-
-        if Model == supported_cell_models[3] and theta > 0:
-            pytest.xfail("failing configuration (but should work)")
-
-        model.stimulus = {0:Expression("1000*t", t=time)}
-
-        info_green("\nTesting %s" % model)
-        vec_solve = self._run_solve(model, time, theta)
-
-        if Model == supported_cell_models[3] and theta == 0:
-            pytest.xfail("failing configuration (but should work)")
-
-        if Model in self.references and theta in self.references[Model]:
-            ind, ref_value = self.references[Model][theta]
-            assert_almost_equal(vec_solve[ind], ref_value, 1e-10)
-        else:
-            info_red("Missing references for %r, %r" % (Model, theta))
