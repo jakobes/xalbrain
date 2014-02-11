1e65e2f84a065bf12ff7a315d6ace3813b17dcb9
Marie E. Rognes <meg@simula.no>
2014-02-11 12:01:26 +0100

    Add configuration file for tests

---
+++ test/conftest.py
@@ -0,0 +1,13 @@
+import sys
+import os
+import pytest
+
+# Disable the xdist plugin by running py.test like this:
+# py.test -p no:xdist
+
+# Automatically parallelize over all cpus
+def pytest_cmdline_preparse(args):
+    if 'xdist' in sys.modules: # pytest-xdist plugin
+        import multiprocessing
+        num = multiprocessing.cpu_count()
+        args[:] = ["-n", str(num)] + args
