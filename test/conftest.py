373a91a2ccdcaa81b186ed2fc19adcf15d3af0dd
Simon Funke <simon.funke@gmailcom>
2014-02-12 11:41:20 +0100

    Clean up the setup hook. Is more general and much cleaner now.

--- test/conftest.py
+++ test/conftest.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import pytest
+import beatadjoint
 
 # Disable the xdist plugin by running py.test like this:
 # py.test -p no:xdist
@@ -13,25 +14,13 @@ def pytest_cmdline_preparse(args):
         args[:] = ["-n", str(num)] + args
 
 
+default_params = beatadjoint.parameters.copy()
 def pytest_runtest_setup(item):
-    """ Hook function  which is called before every test """
+    """ Hook function which is called before every test """
 
-    import beatadjoint
     # Reset dolfin parameter dictionary
-    # FIXME: Is there a better way of doing this?
-    beatadjoint.parameters["form_compiler"]["optimize"] = False
-    beatadjoint.parameters["form_compiler"]["cpp_optimize"] = False
-    beatadjoint.parameters["reorder_dofs_serial"] = True
-    beatadjoint.parameters["form_compiler"]["cpp_optimize_flags"] = '-O2'
-    try:
-        beatadjoint.parameters["adjoint"]["stop_annotating"] = False
-        beatadjoint.parameters["adjoint"]["record_all"] = True
-    except KeyError:
-        pass
+    beatadjoint.parameters.update(default_params)
 
-    try:
-        # Reset adjoint tape
-        from beatadjoint import adj_reset
-        adj_reset()
-    except ImportError:
-        pass
+    # Reset adjoint state
+    if beatadjoint.dolfin_adjoint:
+        beatadjoint.adj_reset()
