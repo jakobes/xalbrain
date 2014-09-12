b0093c8ce65d865ceaab779742220b0417c024ff
Marie E. Rognes <meg@simula.no>
2014-09-12 14:31:58 +0200

    Demo seems to run fine in parallel, data retrieval does not. Awaiting Q&A response.

--- demo/sensitivity-to-conductivities/extract_results.py
+++ demo/sensitivity-to-conductivities/extract_results.py
@@ -1,19 +1,15 @@
-"""
-Sample script to extract data from HDF5 timeseries.
-"""
+# Sample script to extract and plot data from HDF5 timeseries.
 
 import os
 import sys
 from dolfin import *
 
+# Extract results directory from input argument
 assert len(sys.argv) > 1, "Usage: python %s results-dir" % sys.argv[0]
-
 directory = sys.argv[1]
 
-parameters["reorder_dofs_serial"] = False
-
 mesh = Mesh("data/mesh115_refined.xml.gz")
-num_states = 18
+num_states = 1
 V = FunctionSpace(mesh, "CG", 1)
 S = FunctionSpace(mesh, "CG", 1)
 VS = VectorFunctionSpace(mesh, "CG", 1, dim=num_states + 1)
@@ -30,9 +26,10 @@ u_series = TimeSeriesHDF5(mesh.mpi_comm(), "%s/u" % directory)
 times = vs_series.vector_times()
 print "Found %d data samples in the series." % len(times)
 
-for t in times:
-    vs_series.retrieve(vs.vector(), t)
-    u_series.retrieve(u.vector(), t)
+for (i, t) in enumerate(times):
+    print "t = ", t
+    vs_series.retrieve(vs.vector(), t, False)
+    u_series.retrieve(u.vector(), t, False)
     vs0 = vs.split(deepcopy=True)[0]
     vs1 = vs.split(deepcopy=True)[1]
     v.assign(vs0)
