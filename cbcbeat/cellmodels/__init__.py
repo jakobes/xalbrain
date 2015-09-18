a1cb30ca36cee3dbf576b9c2ea7e55763fb2c509
Siri Kallhovd <sirikall@simula.no>
2015-09-18 14:08:05 +0200

    Adding Fenton-Karma models to cbcbeat/cellmodels/__init__.py

--- cbcbeat/cellmodels/__init__.py
+++ cbcbeat/cellmodels/__init__.py
@@ -13,7 +13,8 @@ from nocellmodel import NoCellModel
 from rogers_mcculloch_manual import RogersMcCulloch
 from tentusscher_2004_mcell import Tentusscher_2004_mcell
 from tentusscher_panfilov_2006_epi_cell import Tentusscher_panfilov_2006_epi_cell
-
+from fenton_karma_1998_BR_altered import Fenton_karma_1998_BR_altered
+from fenton_karma_1998_MLR1_altered import Fenton_karma_1998_MLR1_altered
 # Only add supported cell model here if it is tested to actually run
 # with some multistage discretization
 supported_cell_models = (FitzHughNagumoManual,
@@ -21,7 +22,9 @@ supported_cell_models = (FitzHughNagumoManual,
                          RogersMcCulloch,
                          Beeler_reuter_1977,
                          Tentusscher_2004_mcell,
-                         Tentusscher_panfilov_2006_epi_cell)
+                         Tentusscher_panfilov_2006_epi_cell,
+                         Fenton_karma_1998_MLR1_altered,
+                         Fenton_karma_1998_BR_altered)
 
 # Iterate over modules and collect CardiacCellModels
 #supported_cell_models = set()
