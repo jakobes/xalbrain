7c6ac2fd31ad5a7e2f66fbb9498ce5c006c81ec8
Marie E. Rognes <meg@simula.no>
2015-12-07 11:27:15 +0100

    Make first example a bit more interesting and more educational.

--- demo/first-example/demo_first_example.py
+++ demo/first-example/demo_first_example.py
@@ -11,27 +11,36 @@
 # Import the cbcbeat module
 from cbcbeat import *
 
-# Define the cardiac model including the computational domain,
-# conductivities, cell model, stimulus
+# Define the computational domain
 mesh = UnitSquareMesh(100, 100)
 time = Constant(0.0)
+
+# Define the conductivity (tensors)
 M_i = 1.0
 M_e = 1.0
-cell_models = FitzHughNagumoManual()
+
+# Define the cell model and update parameters if you wish to do so
+cell_model = FitzHughNagumoManual()
+cell_model_parameters = cell_model.parameters()
+cell_model_parameters["a"] = Expression("0.13*(10*x[0] + 1.0)", degree=1)
+
+# Define any external stimulus
 stimulus = Expression("10*t*x[0]", t=time)
-cardiac_model = CardiacModel(mesh, time, M_i, M_e, cell_models, stimulus)
+
+# Collect all this information into the CardiacModel class
+cardiac_model = CardiacModel(mesh, time, M_i, M_e, cell_model, stimulus)
+
+# Decide on some numerical scheme options:
+ps = SplittingSolver.default_parameters()
+ps["theta"] = 1.0                        # Use first order splitting
+ps["CardiacODESolver"]["scheme"] = "RL1" # Use Rush-Larsen scheme
 
 # Initialize the solver
-
-# Illustrate how to set parameters
-ps = SplittingSolver.default_parameters()
-#ps["BidomainSolver"]["linear_solver_type"] = "direct"
-#ps["BidomainSolver"]["use_avg_u_constraint"] = True
 solver = SplittingSolver(cardiac_model, params=ps)
 
 # Extract the solution fields and set the initial conditions
 (vs_, vs, vur) = solver.solution_fields()
-vs_.assign(cell_models.initial_conditions())
+vs_.assign(cell_model.initial_conditions())
 
 # Solve
 dt = 0.1
@@ -43,6 +52,8 @@ for (timestep, fields) in solver.solve(interval, dt):
 # Visualize some results
 plot(vs[0], title="Transmembrane potential (v) at end time")
 plot(vs[1], title="State variable (s) at end time")
+
+print vs.vector().norm("l2")
 
 print "Success."
 interactive()
