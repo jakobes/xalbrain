341f4ae8166a1c7b4e28e9a4132c9867f36b2e9f
Marie E. Rognes <meg@simula.no>
2015-12-09 10:28:15 +0100

    Add a touch more illustrating demo using Grandi model.

--- cbcbeat/cellmodels/__init__.py
+++ cbcbeat/cellmodels/__init__.py
@@ -15,8 +15,11 @@ from tentusscher_2004_mcell import Tentusscher_2004_mcell
 from tentusscher_panfilov_2006_epi_cell import Tentusscher_panfilov_2006_epi_cell
 from fenton_karma_1998_BR_altered import Fenton_karma_1998_BR_altered
 from fenton_karma_1998_MLR1_altered import Fenton_karma_1998_MLR1_altered
+from grandi_pasqualini_bers_2010 import Grandi_pasqualini_bers_2010
+
 # Only add supported cell model here if it is tested to actually run
 # with some multistage discretization
+
 supported_cell_models = (FitzHughNagumoManual,
                          NoCellModel,
                          RogersMcCulloch,
@@ -24,7 +27,8 @@ supported_cell_models = (FitzHughNagumoManual,
                          Tentusscher_2004_mcell,
                          Tentusscher_panfilov_2006_epi_cell,
                          Fenton_karma_1998_MLR1_altered,
-                         Fenton_karma_1998_BR_altered)
+                         Fenton_karma_1998_BR_altered,
+                         Grandi_pasqualini_bers_2010)
 
 # Iterate over modules and collect CardiacCellModels
 #supported_cell_models = set()
