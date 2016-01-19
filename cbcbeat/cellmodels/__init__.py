9dec9f4999ea03ffd30f8272431c01e9b6686110
Marie E. Rognes <meg@simula.no>
2016-01-19 14:38:05 +0100

    Fix bug and start collecting "benchmark" examples from various previous sources.

--- cbcbeat/cellmodels/__init__.py
+++ cbcbeat/cellmodels/__init__.py
@@ -27,7 +27,7 @@ supported_cell_models = (FitzHughNagumoManual,
                          Tentusscher_2004_mcell,
                          Tentusscher_panfilov_2006_epi_cell,
                          Fenton_karma_1998_MLR1_altered,
-                         Fenton_karma_1998_BR_altered),
+                         Fenton_karma_1998_BR_altered)#,
                          #Grandi_pasqualini_bers_2010)
 
 # Iterate over modules and collect CardiacCellModels
