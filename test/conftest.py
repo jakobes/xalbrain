e580be09727dbcff802b0a7087054083712c5266
Marie E. Rognes <meg@simula.no>
2016-09-01 17:50:34 +0200

    Remove obsolete dels for testing (2016.1 now)

--- test/conftest.py
+++ test/conftest.py
@@ -5,12 +5,6 @@ import pytest
 import cbcbeat
 import dolfin_adjoint
 
-del dolfin_adjoint.test_initial_condition_adjoint
-del dolfin_adjoint.test_initial_condition_tlm
-del dolfin_adjoint.test_scalar_parameters_adjoint
-del dolfin_adjoint.test_initial_condition_adjoint_cdiff
-del dolfin_adjoint.test_scalar_parameter_adjoint
-
 default_params = cbcbeat.parameters.copy()
 def pytest_runtest_setup(item):
     """ Hook function which is called before every test """
